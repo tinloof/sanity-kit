e3b28c81ccb22f43925042da06a2e012ce76d2c4
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2024-03-19 12:42:30 -0400

    [sanity-studio] pagesNavigator renaming + add title plugin option

--- packages/sanity-studio/src/plugins/navigator/index.ts
+++ packages/sanity-studio/src/plugins/navigator/index.ts
@@ -5,32 +5,36 @@ import { PagesNavigatorPluginOptions } from "../../types";
 import { createPagesNavigator } from "./components/DefaultPagesNavigator";
 import { createPageTemplates, normalizeCreatablePages } from "./utils";
 
-export const pagesNavigator = definePlugin<PagesNavigatorPluginOptions>(
-  (config) => {
-    const normalizedCreatablePages = normalizeCreatablePages(
-      config.creatablePages
-    );
-    return {
-      name: "tinloof-pages-navigator",
-      schema: {
-        templates: createPageTemplates(normalizedCreatablePages),
-      },
-      plugins: [
-        presentationTool({
-          ...config,
-          components: {
-            unstable_navigator: {
-              component: createPagesNavigator({
-                i18n: config.i18n,
-                creatablePages: normalizedCreatablePages,
-              }),
-              minWidth: config.navigator?.minWidth ?? 320,
-              maxWidth: config.navigator?.maxWidth ?? 480,
-            },
-            ...config.components,
+export const pages = definePlugin<PagesNavigatorPluginOptions>((config) => {
+  const normalizedCreatablePages = normalizeCreatablePages(
+    config.creatablePages
+  );
+  return {
+    name: "tinloof-pages-navigator",
+    schema: {
+      templates: createPageTemplates(normalizedCreatablePages),
+    },
+    plugins: [
+      presentationTool({
+        ...config,
+        title: config.title ?? "Pages",
+        components: {
+          unstable_navigator: {
+            component: createPagesNavigator({
+              i18n: config.i18n,
+              creatablePages: normalizedCreatablePages,
+            }),
+            minWidth: config.navigator?.minWidth ?? 320,
+            maxWidth: config.navigator?.maxWidth ?? 480,
           },
-        }),
-      ],
-    };
-  }
-);
+          ...config.components,
+        },
+      }),
+    ],
+  };
+});
+
+/**
+ * @deprecated use `import { pages } from '@tinloof/sanity-studio'` instead
+ */
+export const pagesNavigator = pages;
