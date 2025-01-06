281f590828622e686c20be01c858cd187861871f
stilyan.tinloof <stilyan.toskov@tinloof.com>
2025-01-06 11:45:29 +0000

    input with characters count component

--- packages/sanity-studio/src/components/index.ts
+++ packages/sanity-studio/src/components/index.ts
@@ -1,2 +1,3 @@
 export { IconSelectComponent } from "./IconSelectComponent";
+export { InputWithCharacterCount } from "./input-with-characters-count";
 export { PathnameFieldComponent } from "./PathnameFieldComponent";
