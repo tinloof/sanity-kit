2f373a8426be801e6af2101794a50368495008f5
stilyan-tinloof <stilyan-tinloof@users.noreply.github.com>
2025-10-09 15:19:38 +0100

    define page and define schema

--- packages/sanity-studio/src/utils/index.ts
+++ packages/sanity-studio/src/utils/index.ts
@@ -1,3 +1,5 @@
+export {default as definePage} from "./define-page";
+export {default as defineSchema} from "./define-schema";
 export {definePathname} from "./definePathname";
 export {localizedItem} from "./localizedItem";
 export {singletonListItem} from "./singleton-list-item";
