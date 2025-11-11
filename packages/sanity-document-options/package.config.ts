0564da78cc79c5840f71bc1233633d3a919246f2
stilyan-tinloof <stilyan-tinloof@users.noreply.github.com>
2025-11-11 14:47:08 +0000

    plugin init

---
+++ packages/sanity-document-options/package.config.ts
@@ -0,0 +1,15 @@
+import {defineConfig} from '@sanity/pkg-utils'
+
+export default defineConfig({
+  dist: 'dist',
+  tsconfig: 'tsconfig.dist.json',
+
+  // Remove this block to enable strict export validation
+  extract: {
+    rules: {
+      'ae-incompatible-release-tags': 'off',
+      'ae-internal-missing-underscore': 'off',
+      'ae-missing-release-tag': 'off',
+    },
+  },
+})
---
+++ packages/sanity-document-options/src/index.ts
@@ -0,0 +1,26 @@
+import {definePlugin} from 'sanity'
+
+interface MyPluginConfig {
+  /* nothing here yet */
+}
+
+/**
+ * Usage in `sanity.config.ts` (or .js)
+ *
+ * ```ts
+ * import {defineConfig} from 'sanity'
+ * import {myPlugin} from 'sanity-plugin-sanity-document-options'
+ *
+ * export default defineConfig({
+ *   // ...
+ *   plugins: [myPlugin()],
+ * })
+ * ```
+ */
+export const myPlugin = definePlugin<MyPluginConfig | void>((config = {}) => {
+  // eslint-disable-next-line no-console
+  console.log('hello from sanity-plugin-sanity-document-options')
+  return {
+    name: 'sanity-plugin-sanity-document-options',
+  }
+})
