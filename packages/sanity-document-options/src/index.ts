e36999c06a10b201ff5b881b85964fef00651f1a
stilyan-tinloof <stilyan-tinloof@users.noreply.github.com>
2025-11-11 15:15:19 +0000

    remove sanity package init

--- packages/sanity-document-options/src/index.ts
+++
@@ -1,9 +0,0 @@
-import {definePlugin} from "sanity";
-
-export const myPlugin = definePlugin(() => {
-  // eslint-disable-next-line no-console
-  console.log("hello from sanity-plugin-sanity-document-options");
-  return {
-    name: "sanity-plugin-sanity-document-options",
-  };
-});
