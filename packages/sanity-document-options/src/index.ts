a8c89630daf918e8ae874509899eec903e7ac351
stilyan-tinloof <stilyan-tinloof@users.noreply.github.com>
2025-11-11 14:57:17 +0000

    fix eslint and prettier

--- packages/sanity-document-options/src/index.ts
+++ packages/sanity-document-options/src/index.ts
@@ -1,26 +1,9 @@
-import {definePlugin} from 'sanity'
+import {definePlugin} from "sanity";
 
-interface MyPluginConfig {
-  /* nothing here yet */
-}
-
-/**
- * Usage in `sanity.config.ts` (or .js)
- *
- * ```ts
- * import {defineConfig} from 'sanity'
- * import {myPlugin} from 'sanity-plugin-sanity-document-options'
- *
- * export default defineConfig({
- *   // ...
- *   plugins: [myPlugin()],
- * })
- * ```
- */
-export const myPlugin = definePlugin<MyPluginConfig | void>((config = {}) => {
+export const myPlugin = definePlugin(() => {
   // eslint-disable-next-line no-console
-  console.log('hello from sanity-plugin-sanity-document-options')
+  console.log("hello from sanity-plugin-sanity-document-options");
   return {
-    name: 'sanity-plugin-sanity-document-options',
-  }
-})
+    name: "sanity-plugin-sanity-document-options",
+  };
+});
