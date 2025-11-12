3c6124454944da8bf6f4d39cb6194980d03bc61b
stilyan-tinloof <stilyan-tinloof@users.noreply.github.com>
2025-11-12 09:58:18 +0000

    hide definepage and definedocument

--- packages/sanity-studio/src/utils/index.ts
+++ packages/sanity-studio/src/utils/index.ts
@@ -1,8 +1,3 @@
-export {
-  default as defineDocument,
-  type DefineDocumentDefinition,
-} from "./define-document";
-export {default as definePage} from "./define-page";
 export {definePathname} from "./definePathname";
 export {localizedItem} from "./localizedItem";
 export {singletonListItem} from "./singleton-list-item";
